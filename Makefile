6603b60f00e42ede738e4aaf15ac65ab2864e296
Bruno <ichigoplayer18@gmail.com>
2023-04-12 17:25:50 +0200

    new structure

--- Makefile
+++
@@ -1,6 +0,0 @@
-compose-run-dev: compose-build compose-run dev-all
-
-compose-build:
-	docker-compose build;
-compose-run:
-	docker-compose up -d;
--- storage/framework/views/2ac713a8d36bd2babc2e5331c60301c9d04fbd16.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/brunobryan/Desktop/Programmazione/gratta_e_vinci_contabilità/gratta_e_vinci/src/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php ENDPATH**/ ?>
